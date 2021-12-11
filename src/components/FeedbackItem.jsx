24f9d0bca59615b944bc5909e75e7d7ec4cb68e2
jesus1812s <jesussalasgil@gmail.com>
2021-12-11 12:10:17 -0400

    cleaning code

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,17 +4,10 @@ function FeedbackItem() {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState('Example Feedback item');
 
-  const handleClick = () => {
-    setRating((prev) => {
-      return prev + 1;
-    });
-  };
-
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Edit</button>
     </div>
   );
 }
